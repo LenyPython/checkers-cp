089d97110469937520d6bf24b6e937165481cd45
LenyPython <buniadyserv@gmail.com>
2021-01-05 19:03:36 +0100

    addin pieces to board

--- data/board.py
+++ data/board.py
@@ -1,5 +1,6 @@
 import pygame as pg
-from .constants import ROWS,BLACK,RED,SQUARE_SIZE
+from .constants import COLS,ROWS,BLACK,RED,SQUARE_SIZE,WHITE
+from .piece import Piece
 
 class Board:
 	def __init__(self):
@@ -13,3 +14,17 @@ class Board:
 		for row in range(ROWS):
 			for col in range(row % 2, ROWS, 2):
 				pg.draw.rect(board, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE,SQUARE_SIZE,SQUARE_SIZE))
+	
+	def placePieces():
+		for row in range(ROWS):
+			self.board.append([])
+			for col in range(COLS):
+				if col % 2 == ((row +1) % 2):
+					if row < 3:
+						self.board[row].append(Piece(row,col,WHITE)
+					elif row >4:
+						self.board[row].append(Piece(row,col,RED)
+					else:
+						self.borad[row].append(0)
+				else:
+					self.board[row].append(0)
