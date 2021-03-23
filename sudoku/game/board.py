b537e1afdea25edd1a065d27cda91230c812e66c
LenyPython <buniadyserv@gmail.com>
2021-03-23 20:35:57 +0100

    starting sudoku solver project

---
+++ sudoku/game/board.py
@@ -0,0 +1,5 @@
+import numpy as np
+class Board:
+	def __init__(self):
+		self.board = np.zeros((9,9))
+
--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,7 +1,23 @@
 import pygame as pg
+from game.constants import WIDTH, HEIGHT, WHITE
 
 
 def main():
+	WIN = pg.display.set_mode((WIDTH, HEIGHT))
+	pg.display.set_caption('Sudoku')
+	FPS = 60
+	clock = pg.time.Clock()
+	run = True
+
+	while run:
+		clock.tick(FPS)
+		WIN.fill(WHITE)
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				run = False
+
+		pg.display.update()
+	pg.quit()
 
 if __name__ == '__main__':
 	main()
