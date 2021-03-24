7dc90ff27576a0ede42518a665c9c79d5b6ffa8e
LenyPython <buniadyserv@gmail.com>
2021-03-24 21:55:55 +0100

    added chose and change field

--- sudoku/game/board.py
+++ sudoku/game/board.py
@@ -1,12 +1,24 @@
 import numpy as np
+import random
 import pygame as pg
 from .constants import WIDTH, HEIGHT, BLACK, GREY
 
 class Board:
 	def __init__(self):
-		self.board = np.zeros((9,9))
+		self.board = self._create_board()
 		self.selected = None
 	
+	def _create_board(self):
+		 board = np.zeros((9,9), dtype=np.int8)
+		 
+		 
+	def select_spot(self, row, col):
+		self.selected = {'x': col, 'y': row}
+	
+	def unselect(self):
+		self.selected = None
+
+
 	def draw_lines(self, win):
 		for i in range(10):
 			pg.draw.line(win, BLACK, (10, 10 + 75 * i), (685, 10 + 75 * i), width=5)
@@ -15,5 +27,27 @@ class Board:
 	def draw_board(self, win):
 		pass
 
+	def valid_solution(board):
+		check = [[], [], [], [], [], [], [], [], []]
+		for i, row in enumerate(board):
+			if not all(n in row for n in range(1,10)): return False
+			if i < 3:
+				check[0] += row[:3]
+				check[1] += row[3:6]
+				check[2] += row[6:]
+			elif i < 6:
+				check[3] += row[:3]
+				check[4] += row[3:6]
+				check[5] += row[6:]
+			else:
+				check[6] += row[:3]
+				check[7] += row[3:6]
+				check[8] += row[6:]
+		for sub in check:
+			if not all(n in sub for n in range(1,10)): return False
+		check = [[x[i] for x in board] for i in range(9)]
+		for sub in check:
+			if not all(n in sub for n in range(1,10)): return False
+		return True
 
 
--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from game.constants import WIDTH, HEIGHT, WHITE
+from game.constants import WIDTH, HEIGHT, WHITE, GREY
 from game.board import Board
 
 
@@ -17,7 +17,21 @@ def main():
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
 				run = False
-
+			if event.type == pg.MOUSEBUTTONDOWN:
+				clickPosition = pg.mouse.get_pos()
+				x_pos, y_pos = clickPosition
+				if 10 < x_pos < 685 and 10 < y_pos < 685:
+					x_pos = (x_pos - 10) // 75
+					y_pos = (y_pos - 10) // 75
+					if not game.selected:
+						game.select_spot(y_pos, x_pos)
+					elif x_pos == game.selected['x'] and y_pos == game.selected['y']:
+						game.unselect()
+					else:
+						game.select_spot(y_pos, x_pos)
+	
+		if game.selected:
+			pg.draw.circle(WIN, GREY, ((game.selected['x'] * 75) + 48, (game.selected['y'] * 75) + 48), 35)
 		game.draw_lines(WIN)
 		pg.display.update()
 	pg.quit()
